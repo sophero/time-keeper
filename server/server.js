b01e0b16e3dc4d5c8ddeebf98dfa01b71735a636
Sophia Di <backwardsxmarathon@gmail.com>
2018-10-23 11:54:09 -0400

    Initial commit

---
+++ server/server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+
+const routes = require('./routes');
+
+const port = process.env.PORT || 5000;
+
+const app = express();
+app.use(bodyParser.json());
+
+routes(app);
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '..', 'client', 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
+  });
+}
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
